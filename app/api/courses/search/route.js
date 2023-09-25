260ccc0557124f4e78de277d77a1ee7c46ba173a
una3325 <a33256448@gmail.com>
2023-09-25 12:20:01 +0900

    4

--- app/api/courses/search/route.js
+++ app/api/courses/search/route.js
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server'
 import courses from '../data.json'
 
 export async function GET(request) {
-  const { searchParms } = new URL(request.url)
-  const query = searchParms.get('query')
+  const { searchParams } = new URL(request.url)
+  const query = searchParams.get('query')
   const filteredCourses = courses.filter((course) => {
     return course.title.toLowerCase().includes(query.toLowerCase())
   })
